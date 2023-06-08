4fee815c98d1e43ce6d436ec96292934ab53a9bf
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-08 18:53:40 -0500

    Add Comment test

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  user = User.create(name: 'User Test', photo: 'link', bio: 'Test Comment model', posts_counter: 0)
+  post = Post.create(author_id: user, title: 'Microverse', text: 'Ruby on Rails module', comments_counter: 0,
+                     likes_counter: 0)
+  subject { Comment.new(user_id: user, post:) }
+
+  before { subject.save }
+
+  it 'should increment comments counter for post' do
+    prev_counter = subject.post.comments_counter
+    subject.increment_post_comments_counter
+    expect(subject.post.comments_counter) == prev_counter + 1
+  end
 end
