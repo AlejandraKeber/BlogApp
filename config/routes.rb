95512e65b5cd502b7926aa467d3791ed4db83c1f
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-12 10:43:58 -0500

    Update posts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
-  get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
 end
