5e9d71b47c2a4cbf40ae446fcf4b00181ae6aa9e
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-12 10:56:55 -0500

    Update routes prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 end
