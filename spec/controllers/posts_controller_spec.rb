3f85fa7801641fd7280af8484116388fa18f9165
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-13 17:25:35 -0500

    Fix linters

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe PostsController, type: :request do
       get "/users/#{user.id}/posts"
       expect(response).to be_successful
     end
- 
+
     it 'renders the index template' do
       get "/users/#{user.id}/posts"
       expect(response).to render_template('index')
@@ -20,21 +20,21 @@ RSpec.describe PostsController, type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
-  
+
   describe 'GET #show' do
-  it 'returns a successful response' do
-    get "/users/#{user.id}/posts/#{post.id}"
-    expect(response).to be_successful
-  end
+    it 'returns a successful response' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to be_successful
+    end
 
-  it 'renders the show template' do
-    get "/users/#{user.id}/posts/#{post.id}"
-    expect(response).to render_template('show')
-  end
+    it 'renders the show template' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template('show')
+    end
 
-  it 'includes the correct placeholder text in the response body' do
-    get "/users/#{user.id}/posts/#{post.id}"
-    expect(response.body).to include('Here is the  posts details for a given user')
+    it 'includes the correct placeholder text in the response body' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include('Here is the  posts details for a given user')
+    end
   end
-end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe UsersController, type: :request do
       get '/users'
       expect(response).to be_successful
     end
- 
+
     it 'renders the index template' do
       get '/users'
       expect(response).to render_template('index')
@@ -17,21 +17,21 @@ RSpec.describe UsersController, type: :request do
       expect(response.body).to include('Here is a list of all users')
     end
   end
-  
+
   describe 'GET #show' do
-  it 'returns a successful response' do
-    get '/users/1'
-    expect(response).to be_successful
-  end
+    it 'returns a successful response' do
+      get '/users/1'
+      expect(response).to be_successful
+    end
 
-  it 'renders the show template' do
-    get '/users/1'
-    expect(response).to render_template('show')
-  end
+    it 'renders the show template' do
+      get '/users/1'
+      expect(response).to render_template('show')
+    end
 
-  it 'includes the correct placeholder text in the response body' do
-    get '/users/1'
-    expect(response.body).to include('Here is the details for a given user')
+    it 'includes the correct placeholder text in the response body' do
+      get '/users/1'
+      expect(response.body).to include('Here is the details for a given user')
+    end
   end
-end
 end
