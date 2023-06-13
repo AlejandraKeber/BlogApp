e803b9ec5b8f39bb58f0bfce4ffa58104957363b
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-13 12:58:51 -0500

    Add Users controller rspec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to be_successful
+    end
+ 
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template('index')
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('Here is a list of all users')
+    end
+  end
+  
+  describe 'GET #show' do
+  it 'returns a successful response' do
+    get '/users/1'
+    expect(response).to be_successful
+  end
+
+  it 'renders the show template' do
+    get '/users/1'
+    expect(response).to render_template('show')
+  end
+
+  it 'includes the correct placeholder text in the response body' do
+    get '/users/1'
+    expect(response.body).to include('Here is the details for a given user')
+  end
+end
+end
