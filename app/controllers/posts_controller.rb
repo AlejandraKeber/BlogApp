10d6006d973891ee27772dc68c7a40370b9945d0
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-15 11:00:20 -0500

    Add route and action for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
 end
