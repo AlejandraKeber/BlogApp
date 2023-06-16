00f8c28db7bed25ca148f58281e2c49de788bf80
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-15 20:37:31 -0500

    Update create method to save the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,19 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @post = @user.posts.create(post_params)
-    redirect_to user_posts_path(@user)
+    post_params = params.require(:post).permit(:title, :text)
+    post = Post.new(post_params)
+    post.user = current_user
+    comments_counter = 0
+    likes_counter = 0
+    respond_to do |format|
+      if post.save
+        format.html { redirect_to user_post_path(current_user, post) }
+        flash[:notice] = 'Post created successfully'
+      else
+        format.html { render :new, locals: { post: post } }
+        flash[:alert] = 'Post not created'
+      end
+    end
   end
 end
