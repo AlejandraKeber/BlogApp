216eb631dfb15bef831f2bd621c6ba6328670a17
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-15 22:20:17 -0500

    Create likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    like_params = params.require(:like).permit(:post_id)
+    like = Like.new(like_params)
+    like.user = current_user
+    like.post = Post.find(like_params[:post_id])
+    respond_to do |format|
+      if like.save
+        Like.increment_counter(:likes_counter, like.post.id)
+        format.html { redirect_to user_post_path(current_user, like.post) }
+        flash[:notice] = 'Like created successfully'
+      else
+        format.html { redirect_to user_post_path(current_user, like.post) }
+        flash[:alert] = 'Like not created'
+      end
+    end
+  end
+end
